989974f7c2c8db4fa5550f7f2e63e5b57283f2f0
Harshita-Agarwal223 <aharshita57@gmail.com>
2026-03-02 13:46:23 +0530

    BasicCalculator

--- src/UserCase4.java
+++ src/UserCase4.java
@@ -1,16 +1,13 @@
-public class UserCase4 {
-    public static void main(String[] args) {
-        String [] lines = new String [7];
-        lines[0] = String.join("", "   ***    ", "   ***    ", "*******   ", "  *****    ");
-        lines[1] = String.join("", " **   **  ", " **   **  ", "**    **  ", "**         ");
-        lines[2] = String.join("", "**     ** ", "**     ** ", "**    **  ", "**         ");
-        lines[3] = String.join("", "**     ** ", "**     ** ", "*******   ", "  *****    ");
-        lines[4] = String.join("", "**     ** ", "**     ** ", "**        ", "       **  ");
-        lines[5] = String.join("", " **   **  ", " **   **  ", "**        ", "**     **  ");
-        lines[6] = String.join("", "   ***    ", "   ***    ", "**        ", "  *****    ");
-        for (int i =0 ; i<7;i++)
-            System.out.println(lines[i]);
-        
+public static void main(String[] args){
+        String [] line = new String[7];
+        line[0]=(String.join(" " ,"   ***   ","   ***   ","******   ","  ***** "));
+        line[1]=(String.join(" "," **   ** "," **   ** ","**    ** ","**       "));
+        line[2]=(String.join(" ","**     **","**     **","**    ** "," **      "));
+        line[3]=(String.join(" ","**     **","**     **","******   ","   ***   "));
+        line[4]=(String.join(" ","**     **","**     **","**       ","      ** "));
+        line[5]=(String.join(" "," **   ** "," **   ** ","**       ","**     **"));
+        line[6]=(String.join(" ","   ***   ","   ***   ","**       ","  *****  "));
+        for(int i=0;i<7;i++){
+            System.out.println(line[i]);
+        }
     }
-    
-}

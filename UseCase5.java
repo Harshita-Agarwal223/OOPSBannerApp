3b8b2863d1ccd85975dce24aef6b931574192712
Harshita-Agarwal223 <aharshita57@gmail.com>
2026-03-17 13:56:17 +0530

    UseCase5

--- UseCase5.java
+++ UseCase5.java
@@ -1,5 +1,5 @@
 public class UseCase5 {
-    //PRINT OOPS TO CONSOLE
+  
     public static void main(String[] args) {
         String[] lines = {
             String.join("", "   ***    ", "   ***    ", "*******   ", "  *****    "),

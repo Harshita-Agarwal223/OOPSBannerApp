18dcfff4ed53d1d19374df17cd3e4d0c7cbc4500
Harshita-Agarwal223 <aharshita57@gmail.com>
2026-03-25 13:56:31 +0530

    UserCase08

--- Computer.java
+++ Computer.java
@@ -1,13 +1,14 @@
-public class Computer{
-       String color="Grey";
+public class computer
+{
+    String color="Grey";
     int ram=16;
     double price=14500;
     void writecode()
     {
         System.out.println("I am learning coding");
     }
-    public static void main (String[] args){
-        Computer com = new Computer();
+    public static void main (String[]args){
+        computer com = new computer();
         System.out.println(com.color);
         System.out.println(com.ram);
     }
---
+++ Student.java
@@ -0,0 +1,14 @@
+class Student
+{
+    public static void main(String[] args) {
+    String s = "Programming";
+    system.out.println(s.length());
+    system.out.println(s.touppercase());
+    system.out.println(s.tolowercase());
+    syatem.out.println(s.substring());
+    system.out.println(s.isEmpty());
+    system.out.println(s.contains());
+    system.out.println(s.indexOf());
+    system.out.println(s.charAt());
+}
+}
